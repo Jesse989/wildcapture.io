f7c359f6f8afe10314649e25983d1f6677e4a6d8
Wilfred Driscoll <wd@wildcapture.co>
2021-05-24 21:35:51 -0700

    adding content pages

---
+++ pages/arc_song1_chefboybonez.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import ReactNoSSR from 'react-no-ssr';
+import dynamic from 'next/dynamic';
+
+const VolumetricPlayer = dynamic(() => import('../components/VolumetricPlayer'), { ssr: false });
+
+export default () => {
+    return (
+        <Fragment>
+            <section className="volumetric__wrapper" >
+              <ReactNoSSR>
+                <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={"/vol/song/chefboybones_third_low.drcs"} videoFilePath={"/vol/song/chefboybonez_third_low.mp4"} />
+              </ReactNoSSR>
+            </section>
+        </Fragment>
+    );
+};
