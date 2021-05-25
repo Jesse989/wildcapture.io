c69df44dd981e099426b6fa617b3526d85331337
Wilfred Driscoll <wd@wildcapture.co>
2021-05-24 22:52:52 -0700

    fixed typo

--- pages/arc_song1_chefboybonez.tsx
+++ pages/arc_song1_chefboybonez.tsx
@@ -9,7 +9,7 @@ export default () => {
         <Fragment>
             <section className="volumetric__wrapper" >
               <ReactNoSSR>
-                <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={"/vol/song/chefboybones_third_low.drcs"} videoFilePath={"/vol/song/chefboybonez_third_low.mp4"} />
+                <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={"/vol/song/chefboybonez_third_low.drcs"} videoFilePath={"/vol/song/chefboybonez_third_low.mp4"} />
               </ReactNoSSR>
             </section>
         </Fragment>
