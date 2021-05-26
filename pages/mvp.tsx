e7c050833e45bdd646d63c472431ddc065d68073
Wilfred Driscoll <wd@wildcapture.co>
2021-05-26 15:14:41 -0700

    fix mvp

--- pages/mvp.tsx
+++ pages/mvp.tsx
@@ -9,7 +9,7 @@ export default () => {
         <Fragment>
             <section className="volumetric__wrapper" >
               <ReactNoSSR>
-                <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={"mahomes_mvp_low.drcs"} videoFilePath={"mahomes_mvp_low.mp4"} />
+                <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={"/mahomes_mvp_low.drcs"} videoFilePath={"/mahomes_mvp_low.mp4"} />
               </ReactNoSSR>
             </section>
         </Fragment>
