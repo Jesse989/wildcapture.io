199f5f2d36017ea52e1f216e920b9116641c5742
Wilfred Driscoll <wd@wildcapture.co>
2021-06-01 23:37:49 -0700

    addingvolpages

---
+++ pages/kungfumed.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import ReactNoSSR from 'react-no-ssr';
+import dynamic from 'next/dynamic';
+
+const VolumetricPlayer = dynamic(() => import('../components/VolumetricPlayer'), { ssr: false });
+
+export default () => {
+    return (
+        <Fragment>
+            <section className="volumetric__wrapper" >
+              <ReactNoSSR>
+                <VolumetricPlayer style={{ width: '100%', height: '100%' }} meshFilePath={"/kungfu_med.drcs"} videoFilePath={"/kungfu_low.mp4"} />
+              </ReactNoSSR>
+            </section>
+        </Fragment>
+    );
+};
