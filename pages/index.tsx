40a766cc7c2407d67d4485d4628393733770dac4
Wilfred Driscoll <wd@wildcapture.co>
2021-06-13 14:25:55 -0700

    fixed homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,6 @@ export default () => {
                 <video className="home-banner-movie" autoPlay loop muted>
                     <source src="../movie/banner-slide-movie.mp4"></source>
                 </video>
-                <video className="home-banner-movie-small" autoPlay loop muted>
-                    <source src="../movie/banner-slide-movie.mp4"></source>
-                </video>
-                <img className="home-banner-large" src={"../img/banner-homepage.jpg"} />
-                <img className="home-banner-small" src={"../img/banner-homepage-small.jpg"} />
             </section>
             <About />
             <Work />
